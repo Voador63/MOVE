874780390b3c0abba0d2267a961545f34ef5d06d
Killian <voador.kiki@gmail.com>
2019-04-12 15:52:22 -0400

    Set up de la custom list view: le build est potentiellement cassé

--- app/src/main/java/com/example/move/Tab3Fragment.java
+++ app/src/main/java/com/example/move/Tab3Fragment.java
@@ -1,68 +1,89 @@
 package com.example.move;
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.move.CustomAdapter;
+import com.example.move.SuccessDataModel;
+
+import java.util.ArrayList;
 
 public class Tab3Fragment extends Fragment {
-    private static final String TAG = "Tab3Fragment";
+
+    ArrayList<SuccessDataModel> dataModels;
+    ListView listView;
+    private static CustomAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.tab3_fragment,container,false);
-        //final LinearLayout linearLayout1 = view.findViewById(R.id.succes1);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        //succes controller
-        final TextView txtInfos1 = view.findViewById(R.id.succesInfos1);
-        txtInfos1.setVisibility(View.GONE);
+        listView=(ListView)view.findViewById(R.id.list);
 
-        final Button btnInfo1 = view.findViewById(R.id.button);
-        btnInfo1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MoreInfo(txtInfos1);
-            }
-        });
+        dataModels= new ArrayList<>();
 
-        //succes2 controller
-        final TextView txtInfos2 = view.findViewById(R.id.succesInfos2);
-        txtInfos2.setVisibility(View.GONE);
+        dataModels.add(new SuccessDataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+        dataModels.add(new SuccessDataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+        dataModels.add(new SuccessDataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+        dataModels.add(new SuccessDataModel("Donut","Android 1.6","4","September 15, 2009"));
+        dataModels.add(new SuccessDataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+        dataModels.add(new SuccessDataModel("Froyo", "Android 2.2", "8","May 20, 2010"));
+        dataModels.add(new SuccessDataModel("Gingerbread", "Android 2.3", "9","December 6, 2010"));
+        dataModels.add(new SuccessDataModel("Honeycomb","Android 3.0","11","February 22, 2011"));
+        dataModels.add(new SuccessDataModel("Ice Cream Sandwich", "Android 4.0", "14","October 18, 2011"));
+        dataModels.add(new SuccessDataModel("Jelly Bean", "Android 4.2", "16","July 9, 2012"));
+        dataModels.add(new SuccessDataModel("Kitkat", "Android 4.4", "19","October 31, 2013"));
+        dataModels.add(new SuccessDataModel("Lollipop","Android 5.0","21","November 12, 2014"));
+        dataModels.add(new SuccessDataModel("Marshmallow", "Android 6.0", "23","October 5, 2015"));
 
-        final Button btnInfo2 = view.findViewById(R.id.button2);
-        btnInfo2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MoreInfo(txtInfos2);
-            }
-        });
+        adapter= new CustomAdapter(dataModels,getActivity().getApplicationContext());
 
-        //succes3 controller
-        final TextView txtInfos3 = view.findViewById(R.id.succesInfos3);
-        txtInfos3.setVisibility(View.GONE);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        final Button btnInfo3 = view.findViewById(R.id.button3);
-        btnInfo3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                MoreInfo(txtInfos3);
+                SuccessDataModel dataModel= dataModels.get(position);
+
+                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
             }
         });
 
         return view;
     }
 
-    private void MoreInfo(TextView text)
-    {
-        if (text.getVisibility() == View.GONE) {
-            text.setVisibility(View.VISIBLE);
-        }
-        else {
-            text.setVisibility(View.GONE);
-        }
-    }
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+    //    // Inflate the menu; this adds items to the action bar if it is present.
+    //    getMenuInflater().inflate(R.menu.menu_main, menu);
+    //    return true;
+    //}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
