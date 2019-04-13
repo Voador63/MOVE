922d7e52f6ced2292125f06bc8bbbac6380caad9
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-04-13 09:36:31 -0400

    Ajout du module de carte
    Le fragment 1 a été remplacé par le fragment de la carte, et la carte est visible (si, sisi)

--- app/src/main/java/com/example/move/MainActivity.java
+++ app/src/main/java/com/example/move/MainActivity.java
@@ -1,26 +1,10 @@
 package com.example.move;
 
-import android.os.Debug;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Tab1Fragment(), getResources().getString(R.string.fragment1));
+        adapter.addFragment(new MapsFragment(), getResources().getString(R.string.fragment1));
         adapter.addFragment(new Tab2Fragment(), getResources().getString(R.string.fragment2));
         adapter.addFragment(new Tab3Fragment(), getResources().getString(R.string.fragment3));
         viewPager.setAdapter(adapter);
