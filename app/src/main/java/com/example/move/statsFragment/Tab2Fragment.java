b58b96f8df6ebc55e2261cd4b1710108c5021ada
Killian <voador.kiki@gmail.com>
2019-04-21 21:41:36 -0400

    Instanciation de l'objet Stats, affichage de ses valeurs en fonction de la BD. Création des fonctiones permettant de valider le premier succès et de mettre à jour le stats.

--- app/src/main/java/com/example/move/statsFragment/Tab2Fragment.java
+++ app/src/main/java/com/example/move/statsFragment/Tab2Fragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.move.R;
+import com.example.move.data.StatsDAO;
 
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
@@ -17,6 +19,20 @@ public class Tab2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.tab2_fragment,container,false);
+
+        TextView dist = (TextView) view.findViewById(R.id.textDistance);
+        dist.setText(Double.toString(StatsDAO.selectAll().get(0).getDist_totale())+"m");
+
+        TextView vit = (TextView) view.findViewById(R.id.textVitesse);
+        vit.setText(Double.toString(StatsDAO.selectAll().get(0).getVitesse_max())+"km/h");
+
+        TextView denivPos = (TextView) view.findViewById(R.id.textDenivPos);
+        denivPos.setText(Double.toString(StatsDAO.selectAll().get(0).getDeniv_pos())+"m");
+
+        TextView denivNeg = (TextView) view.findViewById(R.id.textDenivNeg);
+        denivNeg.setText(Double.toString(StatsDAO.selectAll().get(0).getDeniv_neg()) + "m");
+
+
         return view;
     }
 
